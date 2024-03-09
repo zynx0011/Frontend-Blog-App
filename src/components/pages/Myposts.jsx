6577d39ff14804e903a3499ab8df69d353f5daa1
zynx0011 <kunalnachan10@gmail.com>
2024-03-09 22:26:58 +0530

    aa

--- src/components/pages/Myposts.jsx
+++ src/components/pages/Myposts.jsx
@@ -2,7 +2,7 @@ import { Container } from "@mui/material";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { PostCard } from "../PostCardPg";
+import { PostCard } from "..";
 import CircularProgress from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
 import { API_URL } from "../../config";
