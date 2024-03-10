bdd936c7a9e455bc7565a129a464df70aa678614
zynx0011 <kunalnachan10@gmail.com>
2024-03-10 20:19:08 +0530

    added logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Logo({ width = "100px" }) {
-  return <img src="logo.png" alt="" width={"80px"} className="rounded-lg" />;
+  return (
+    <img
+      src="https://blog.logrocket.com/wp-content/uploads/2021/04/express-js-template-engines.png"
+      alt=""
+      width={"80px"}
+      className="rounded-lg"
+    />
+  );
 }
 
 export default Logo;
