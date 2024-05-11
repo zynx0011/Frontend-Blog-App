e583311d8adf425d8bcb31bb64421aecd4a19ecf
zynx0011 <kunalnachan10@gmail.com>
2024-05-11 13:39:40 +0530

    solve error

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Logo({ width = "100px" }) {
   return (
     <img
-      src="https://blog.logrocket.com/wp-content/uploads/2021/04/express-js-template-engines.png"
+      src="/expressblog.png"
       alt="Logo"
       className="rounded-lg w-[80px] h-auto"
     />
